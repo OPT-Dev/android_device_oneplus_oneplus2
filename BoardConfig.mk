8ab3d1dff0c20481e733c149d471f6688a4269c5
David Vaz Guijarro <vazdavid@icloud.com>
2015-11-09 18:28:50 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,7 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
-# Wifi
+# WiFi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
 BOARD_WLAN_DEVICE := qcwcn
@@ -198,9 +198,6 @@ include device/qcom/sepolicy/sepolicy.mk
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
-
-#Disable HW based full disk encryption
-TARGET_HW_DISK_ENCRYPTION := false
 
 # Keymaster
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
